0e4578c116dab395892a27b3edd3dc3324b8ae36
Luiz Contri <luiz.contri123@gmail.com>
2021-01-31 13:07:32 -0300

    Added User Model and Resources

--- REST APIs com Flask/Projeto Hoteis/app.py
+++ REST APIs com Flask/Projeto Hoteis/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import  Api
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import User, Hotel
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -13,6 +15,9 @@ def cria_banco():
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resourece(User, '/usuarios/<int:user_id>')
+
+
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
